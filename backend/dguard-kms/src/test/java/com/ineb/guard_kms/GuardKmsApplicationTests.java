c8e0bf774c9a81c4eaa4bcb063151ea454351a08
Devlsj <dltjdwo0404@gmail.com>
2026-08-19 10:44:58 +0900

    초기 프로젝트 구조 및 CI/CD 환경 설정

---
+++ backend/dguard-kms/src/test/java/com/ineb/guard_kms/GuardKmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ineb.guard_kms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardKmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
